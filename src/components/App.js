97cdb56818f518e65d45b8cc23c7968350fd71a1
Jamie H Pham <jpham1109@gmail.com>
2021-04-04 20:16:58 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from Header.js
+import Article from Article.js
 
 function App() {
   return <div className="App">learn react!</div>;
+  <Header />
+  <Article />
 }
 
 export default App;
